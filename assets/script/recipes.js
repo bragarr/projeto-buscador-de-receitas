25f7a3f3e293b4baf707e8b90ea3ae1638f3ee35
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-06 15:54:04 -0400

    new content on html and JS

--- assets/script/recipes.js
+++ assets/script/recipes.js
@@ -7,6 +7,7 @@ recipeFormn.addEventListener("submit", (event) => {
     const inputValue = userInput.value;
     console.log(inputValue);
     const recipesList = document.getElementById("result__list");
+    const mainContent = document.querySelector("main");
     const apiUrl = `https://www.themealdb.com/api/json/v1/${apiKey}/search.php?s=${inputValue}`;
     fetch(apiUrl)
         .then(response => response.json())
@@ -21,13 +22,35 @@ recipeFormn.addEventListener("submit", (event) => {
                     <img src="${meals[i].strMealThumb}" alt="Meal Thumnail" class="recipes__image">
                     <h3 class="name__recipes">${meals[i].strMeal}</h3>
                 </figure>
-                <p>${meals[i].strArea}</p>
-                <aside class="buttons_interface">
-                    <button class="button__check">Check Recipe</button>
-                </aside>
+                <figure class="container__country">
+                    <img src="./assets/img/${meals[i].strArea}.png" alt="Recipe Country" class="flag__image">
+                    <h2 class="name__flag">${meals[i].strArea}</h2>
+                </figure>
+                <button class="button__check" value=${i}>OPEN</button>
                 `;
                 li.innerHTML = containerResultSearch;
                 recipesList.appendChild(li);
             }
+
+            const buttons = document.querySelectorAll(".button__check");
+            buttons.forEach(button => {
+                button.addEventListener("click", () => {
+                    const recipeDetail = document.createElement("section");
+                    const container = document.createElement("div");
+                    recipeDetail.classList.add("recipe__detail");
+                    const recipeIngridients = `
+                    <p>
+                        ${meals[button.value].strInstructions}
+                    </p>
+                    `;
+                    container.innerHTML = recipeIngridients;
+                    recipeDetail.appendChild(container);
+                    mainContent.appendChild(recipeDetail);
+                    const itemList = document.querySelectorAll(".result__meal");
+                    itemList.forEach(item => {
+                        recipesList.removeChild(item);
+                    })
+                })
+            })
         })
 })
