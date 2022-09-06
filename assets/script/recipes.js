7f0d51bce2968cabc788052a88ce605b470c690c
Reinaldo Braga <rodrigues.reinaldobraga@outlook.com>
2022-09-05 23:37:44 -0400

    Update recipes.js

--- assets/script/recipes.js
+++ assets/script/recipes.js
@@ -21,6 +21,7 @@ recipeFormn.addEventListener("submit", (event) => {
                     <img src="${meals[i].strMealThumb}" alt="Meal Thumnail" class="recipes__image">
                     <h3 class="name__recipes">${meals[i].strMeal}</h3>
                 </figure>
+                <p>${meals[i].strArea}</p>
                 <aside class="buttons_interface">
                     <button class="button__check">Check Recipe</button>
                 </aside>
